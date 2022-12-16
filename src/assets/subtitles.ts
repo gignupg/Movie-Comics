d5af0185e09344750bfd15c2662f49cbf9c0705b
gignu <gignupg@gmail.com>
2022-12-17 00:47:20 +0100

    Added German translations and restructured assets

---
+++ src/assets/subtitles.ts
@@ -0,0 +1,138 @@
+const subtitles = {
+    text1: {
+        en: ["My bad! Everybody all right?"],
+        es: ["¡Culpa mía! ¿Todos bien?"],
+        de: ["Mein Fehler! Alle in Ordnung?"]
+    },
+    text2: {
+        en: ["He's an asshole!"],
+        es: ["¡Es un imbécil!"],
+        de: ["Er ist ein Arschloch!"]
+    },
+    text3: {
+        en: ["- What's your name, boy?", "- Michel."],
+        es: ["- ¿Cómo te llamas, chico?", "- Michel."],
+        de: ["- Wie ist dein Name, Junge?", "- Michel."]
+    },
+    text4: {
+        en: ["- (Do) you know who I am?", "- My papa says you are an asshole!"],
+        es: ["- ¿Sabes quién soy?", "- ¡Mi papá dice que eres un imbécil!"],
+        de: ["- Weißt du wer ich bin?", "- Mein Papa sagt, du bist ein Arschloch!"]
+    },
+    text5: {
+        en: ["That's not really a nice word to call a person, is it?"],
+        es: ["Esa no es realmente una buena palabra para llamar a una persona, ¿verdad?"],
+        de: ["Das ist nicht wirklich ein nettes Wort, eine Person so zu rufen, oder?"]
+    },
+    text6: {
+        en: ["- Asshole?", "- Yeah, cause that could make someone very angry!"],
+        es: ["- ¿Imbécil?", "- ¡Sí, porque eso podría enojar mucho a alguien!"],
+        de: ["- Arschloch?", "- Ja, weil das könnte jemanden sehr wütend machen!"]
+    },
+    text7: {
+        en: ["- And you know, maybe hurt their feelings!", "- You behave like an asshole!"],
+        es: ["- Y ya sabes, ¡quizás herir sus sentimientos!", "- ¡Te comportas como un imbécil!"],
+        de: ["- Und weißt du, vielleicht seine Gefühle verletzen!", "- Du benimmst dich wie ein Arschloch!"]
+    },
+    text8: {
+        en: ["- All right, uhh, (do you) you know Aaron?", "- Oui, le petit asshole."],
+        es: ["- Está bien, ¿conoces a Aaron?", "Sí, el pequeño imbécil?"],
+        de: ["- In Ordnung, äh, (kennst du) Aaron?", "Ja, das kleine Arschloch?"]
+    },
+    text9: {
+        en: ["Well uhh, (he) seems to be a pretty good kid uhh."],
+        es: ["Pues, parece ser un niño bastante bueno."],
+        de: ["Nun, äh, (er) scheint ein ziemlich guter Junge zu sein."]
+    },
+    text10: {
+        en: ["- (I) just wanna ask you to lay off of him a little bit.", "- Why asshole?"],
+        es: ["- Solo quiero pedirte que lo dejes un poco.", "- ¿Por qué imbécil?"],
+        de: ["- Ich möchte dich nur bitten ihn ein bisschen in Ruhe zu lassen.", "- Warum Arschloch?"]
+    },
+    text11: {
+        en: ["- You (are) gonna stop calling me that!", "- Asshole!"],
+        es: ["- ¡Vas a dejar de llamarme así!", "- Imbécil!"],
+        de: ["- Du wirst aufhören, mich so zu nennen!", "- Arschloch!"]
+    },
+    text12: {
+        en: ["- That's not my name!", "- Asshole!"],
+        es: ["- ¡Ese no es mi nombre!", "- Imbécil!"],
+        de: ["- Das ist nicht mein Name!", "- Arschloch!"]
+    },
+    text13: {
+        en: ["Call me a(n) asshole, one more time!"],
+        es: ["¡Llámame imbécil, una vez más!"],
+        de: ["Nenn mich noch einmal Arschloch!"]
+    },
+    text14: {
+        en: ["(What) about you thickness?"],
+        es: ["¿Qué hay de ti, gordo?"],
+        de: ["Was ist mit dir, Fettsack?"]
+    },
+    text15: {
+        en: ["Goggles?"],
+        es: ["¿Gafas?"],
+        de: ["Brillenschlange?"]
+    },
+    text16: {
+        en: ["Hancock, you son of a gun, I knew you'd come, ask Mary!"],
+        es: ["Hancock, hijo de puta, sabía que vendrías, ¡pregúntaselo a Mary!"],
+        de: ["Hancock, du Gauner, ich wusste du würdest kommen, frag Mary!"]
+    },
+    text17: {
+        en: ["I had a feeling, I said he heard me, he's ready for a change!"],
+        es: ["Tuve un presentimiento, dije que me escuchó, ¡está listo para un cambio!"],
+        de: ["Ich hatte es im Gefühl, ich sagte mir, er hat mich gehört, er ist bereit für eine Veränderung!"]
+    },
+    text18: {
+        en: ["This is great! Did you do this? Did you come in a little hot?"],
+        es: ["¡Esto es genial! ¿Tú hiciste esto? ¿Llegaste un poco caliente?"],
+        de: ["Das ist toll! Hast du das gemacht? Bist du ein bisschen heiß reingekommen/gelandet?"]
+    },
+    text19: {
+        en: ["- Yeah, (I'll meet) I'll meet you in the house!", "- This is the very place for us to start, I know you don't drive!", "- Aww, Dammit!"],
+        es: ["- ¡Sí, te veré en la casa!", "- Este es el lugar perfecto para que empecemos, ¡sí que no conduces!", "- ¡Ay, maldita sea!"],
+        de: ["- Ja, ich treffe dich im Haus!", "- Das ist genau der richtige Ort für uns zum Anfangen, ich weiß, du fährst kein Auto!", "- Oh, verdammt!"]
+    },
+    text20: {
+        en: ["All right, you're all right, you're all right, you're all right!"],
+        es: ["¡Está bien, está bien, está bien, está bien!"],
+        de: ["In Ordnung, alles in Ordnung, alles in Ordnung, alles in Ordnung!"]
+    },
+    text22: {
+        en: ["Oh stop crying punkass, go ahead!"],
+        es: ["Oh, deja de llorar idiota, ¡adelante!"],
+        de: ["Oh, hör auf zu heulen du Kobold, vorwärts!"]
+    },
+    text23: {
+        en: ["(This is) not ok, ok? (This is) really not ok!"],
+        es: ["Esto no está bien, ¿de acuerdo? ¡Esto realmente no está bien!"],
+        de: ["Das ist nicht ok, ok? Das ist wirklich nicht ok!"]
+    },
+    text24: {
+        en: ["This is some of the stuff we're gonna work on!"],
+        es: ["¡Estas son algunas de las cosas en las que vamos a trabajar!"],
+        de: ["Das sind einige der Dinge, an denen wir arbeiten werden!"]
+    },
+    text25: {
+        en: ["How do you think that conversation is gonna go down with his mom?"],
+        es: ["¿Cómo crees que irá esa conversación con su mamá?"],
+        de: ["Wie denkst du, wird das Gespräch mit seiner Mutter laufen?"]
+    },
+    text26: {
+        en: ["Landing like that on the street is also on the uncool side!"],
+        es: ["¡Aterrizar así en la calle también está en el lado desagradable!"],
+        de: ["So auf der Straße zu landen ist auch uncool!"]
+    },
+    text27: {
+        en: ["No that was, that was already like that when I got here, Ray!"],
+        es: ["¡No, eso ya era así cuando llegué aquí, Ray!"],
+        de: ["Nein, das war schon so, als ich hierher kam, Ray!"]
+    },
+    text28: {
+        en: ["I live here, I know what the street's like!"],
+        es: ["¡Vivo aquí, sé cómo es la calle!"],
+        de: ["Ich wohne hier, ich kenne die Straße!"]
+    },
+};
+export default subtitles;
---
+++ src/assets/text.ts
@@ -0,0 +1,162 @@
+import subtitles from './subtitles';
+const lang = 'en';
+const region = 'de';   // 'de'
+export const text1 = {
+    subtitles: subtitles.text1[lang],
+    translation: subtitles.text1[region],
+    vocabulary: [
+        ["My bad", "Used to acknowledge responsibility for a mistake. \"Sorry about the confusion. It's my bad!\""]
+    ]
+};
+export const text2 = {
+    subtitles: subtitles.text2[lang],
+    translation: subtitles.text2[region],
+    vocabulary: []
+};
+export const text3 = {
+    subtitles: subtitles.text3[lang],
+    translation: subtitles.text3[region],
+    vocabulary: []
+};
+export const text4 = {
+    subtitles: subtitles.text4[lang],
+    translation: subtitles.text4[region],
+    vocabulary: []
+};
+export const text5 = {
+    subtitles: subtitles.text5[lang],
+    translation: subtitles.text5[region],
+    vocabulary: []
+};
+export const text6 = {
+    subtitles: subtitles.text6[lang],
+    translation: subtitles.text6[region],
+    vocabulary: []
+};
+export const text7 = {
+    subtitles: subtitles.text7[lang],
+    translation: subtitles.text7[region],
+    vocabulary: []
+};
+export const text8 = {
+    subtitles: subtitles.text8[lang],
+    translation: subtitles.text8[region],
+    vocabulary: [
+        ["Oui [French]", "Yes"],
+        ["Petit [French]", "Small"],
+        ["Le petit asshole [French/English]", "The little asshole (the kid is mixing French and English)"]
+    ]
+};
+export const text9 = {
+    subtitles: subtitles.text9[lang],
+    translation: subtitles.text9[region],
+    vocabulary: []
+};
+export const text10 = {
+    subtitles: subtitles.text10[lang],
+    translation: subtitles.text10[region],
+    vocabulary: [
+        ["To lay off of someone", "To leave someone alone, to stop bothering/attacking/hurting someone"]
+    ]
+};
+export const text11 = {
+    subtitles: subtitles.text11[lang],
+    translation: subtitles.text11[region],
+    vocabulary: []
+};
+export const text12 = {
+    subtitles: subtitles.text12[lang],
+    translation: subtitles.text12[region],
+    vocabulary: []
+};
+export const text13 = {
+    subtitles: subtitles.text13[lang],
+    translation: subtitles.text13[region],
+    vocabulary: []
+};
+export const text14 = {
+    subtitles: subtitles.text14[lang],
+    translation: subtitles.text14[region],
+    vocabulary: [
+        ["Thickness", "Used as a swear word, meaning fat (non-standard)"],
+        ["About you thickness?", "What about you fat boy?"]
+    ]
+};
+export const text15 = {
+    subtitles: subtitles.text15[lang],
+    translation: subtitles.text15[region],
+    vocabulary: [
+        ["Goggles", "Special glasses that fit close to the face to protect the eyes, often used for swimming"]
+    ]
+};
+export const text16 = {
+    subtitles: subtitles.text16[lang],
+    translation: subtitles.text16[region],
+    vocabulary: [
+        ["Son of a gun", "Son of a gun is an exclamation in American and British English. It can be used encouragingly or to compliment, as in \"You son of a gun, you did it!\""]
+    ]
+};
+export const text17 = {
+    subtitles: subtitles.text17[lang],
+    translation: subtitles.text17[region],
+    vocabulary: []
+};
+export const text18 = {
+    subtitles: subtitles.text18[lang],
+    translation: subtitles.text18[region],
+    vocabulary: [
+        ["To come in hot", "He's refering to Hancock's landing, the street got a little destroyed when he landed"]
+    ]
+};
+export const text19 = {
+    subtitles: subtitles.text19[lang],
+    translation: subtitles.text19[region],
+    vocabulary: [
+        ["Very", "Exact/precise"],
+        ["You don't drive", "Hancock doesn't drive or own a car because he always flies"]
+    ]
+};
+export const text20 = {
+    subtitles: subtitles.text20[lang],
+    translation: subtitles.text20[region],
+    vocabulary: []
+};
+export const text22 = {
+    subtitles: subtitles.text22[lang],
+    translation: subtitles.text22[region],
+    vocabulary: [
+        ["Punkass", "An obnoxious or stupid person unworthy of respect"]
+    ]
+};
+export const text23 = {
+    subtitles: subtitles.text23[lang],
+    translation: subtitles.text23[region],
+    vocabulary: []
+};
+export const text24 = {
+    subtitles: subtitles.text24[lang],
+    translation: subtitles.text24[region],
+    vocabulary: []
+};
+export const text25 = {
+    subtitles: subtitles.text25[lang],
+    translation: subtitles.text25[region],
+    vocabulary: [
+        ["To go down", "To be received in a particular way: \"The joke didn't go down very well!\""]
+    ]
+};
+export const text26 = {
+    subtitles: subtitles.text26[lang],
+    translation: subtitles.text26[region],
+    vocabulary: []
+};
+export const text27 = {
+    subtitles: subtitles.text27[lang],
+    translation: subtitles.text27[region],
+    vocabulary: []
+};
+export const text28 = {
+    subtitles: subtitles.text28[lang],
+    translation: subtitles.text28[region],
+    vocabulary: []
+};
